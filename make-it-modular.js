a236a74c110a192646c7160fc31a1692380d8763
Benjamin Kyhn <benkyhn@hotmail.com>
2020-09-09 14:00:17 +0200

    update

--- make-it-modular.js
+++ make-it-modular.js
@@ -1,3 +1,10 @@
-const myModule = require('./mymodule.js')
+var mymodule = require('./mymodule.js')
 
-myModule.listFiles();
+var dir = process.argv[2];
+var ext = process.argv[3];
+
+mymodule(dir, ext, function(err, list){
+    for (let index = 0; index < list.length; index++) {
+        console.log(list[index].toString());        
+    }
+})
--- mymodule.js
+++ mymodule.js
@@ -1,14 +1,15 @@
-const fs = require('fs')
-const path = require('path')
+var fs = require('fs')
+var path = require('path')
 
-const dir = process.argv[2];
-const ext = "." + process.argv[3];
-
-const listFiles = fs.readdir(dir, ext, function callback(err, list){
-    for (let index = 0; index < list.length; index++) {
-        if (path.extname(list[index]) === ext)
-        console.log(list[index].toString());
-    }
-})
-
-module.exports = listFiles;
+module.exports = function(dir, ext, callback){
+    fs.readdir(dir, function(err, list){
+        var filteredList = [];
+        var ext = "." + ext;
+        for (let index = 0; index < list.length; index++) {
+            if (path.extname(list[index]) === ext){
+                filteredList.push(list[index]);
+            };  
+        }
+        return callback(null, filteredList);
+    });
+};
