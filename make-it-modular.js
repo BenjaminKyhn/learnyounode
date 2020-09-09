c54e6282aba26de7dce7e11712667e4dddbda0f9
Benjamin Kyhn <benkyhn@hotmail.com>
2020-09-09 14:23:19 +0200

    changes

--- make-it-modular.js
+++ make-it-modular.js
@@ -6,6 +6,7 @@ let ext = process.argv[3];
 mymodule(dir, ext, function(err, list){
     if (err) 
         return console.error(err);
+
         for (let index = 0; index < list.length; index++) {
             console.log(list[index]);
         }
--- mymodule.js
+++ mymodule.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 const path = require('path')
 
-module.exports = function(dir, fileFilter, callback){
+module.exports = function(dir, ext, callback){
     fs.readdir(dir, function(err, list){
         if (err) 
             return callback(err);
         let filteredList = [];
-        let ext = '.' + fileFilter;
+        let fileFilter = '.' + ext;
         
         list.forEach(function (file) {
-            if (path.extname(file) === ext) {
+            if (path.extname(file) === fileFilter) {
                 filteredList.push(file);
             }
         });
